df149e3de88139d5d7a0b5b11f4bb0f660b1565e
JoaquinLagares <joacoglagares@gmail.com>
2026-03-09 14:25:53 -0300

    fix db connection

--- db/pool.js
+++ db/pool.js
@@ -12,11 +12,11 @@ const { Pool } = require("pg");
 // });
 
 
+require("dotenv").config();
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 module.exports = pool;
